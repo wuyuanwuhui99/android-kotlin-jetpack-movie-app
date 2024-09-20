c6a17f1bf1ce8871993ace1d32fabfb4e269e3d1
wuyuanwuhui99 <275018723@qq.com>
2024-09-20 19:11:37 +0800

    修改配置

--- app/src/main/java/com/player/constant/Constant.kt
+++ app/src/main/java/com/player/constant/Constant.kt
@@ -1,6 +1,5 @@
 package com.player.constant
 
 object Constant {
-    const val HOST = "http://192.168.0.5:5001/"
-//    const val HOST = "http://254a2y1767.qicp.vip"
+    const val HOST = "http://169.254.32.117:5001"
 }
